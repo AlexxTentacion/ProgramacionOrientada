d6a2eb1f12fa1fe1f84f4a8eb790ed8d8563cc56
AlexxTentacion <89532931+AlexxTentacion@users.noreply.github.com>
2022-08-27 03:04:14 -0500

    calculadora

--- Main.java
+++ Main.java
@@ -1,7 +1,49 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("bye world");
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+float PrimerN;
+float SegundoN;
+char action;
+float resp;
+
+System.out.println("Ingrese el primer numero/s");
+PrimerN = Integer.parseInt(entrada.readLine());
+
+        System.out.println("Ingrese el segundo numero/s");
+        SegundoN = Integer.parseInt(entrada.readLine());
+
+        System.out.println("Ingrese la operacion a realizar: +, -, *, /");
+        action = (char) System.in.read();
+
+        switch (action) {
+            case '+' -> {
+                resp = PrimerN + SegundoN;
+                break;
+            }
+            case '-' -> {
+                resp = PrimerN - SegundoN;
+                break;
+            }
+            case '*' -> {
+                resp = PrimerN * SegundoN;
+                break;
+            }
+            case '/' -> {
+                resp = PrimerN % SegundoN;
+                break;
+            }
+            default -> {
+                System.out.println("Incorrecto!!, eso no es una operacion");
+                return;
+            }
+        }
+        System.out.println("Resultados:");
+        System.out.println(PrimerN + "" + action + "" + SegundoN + "=" + resp);
+    }
 }

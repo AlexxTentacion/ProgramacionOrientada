a75214b05bf6ce1c38d7464597583b4feedd7463
AlexxTentacion <89532931+AlexxTentacion@users.noreply.github.com>
2022-08-27 22:09:08 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("bye world");
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        int a = 100; //boleto adulto
+        int n = 70; //boleto niño
+        int i = 50; //adulto mayor con credencial del INAPAM
+        int b = 70; //adulto mayor sin credencial del INAPAM
+        char opcion;
+        char tipos;
+
+        System.out.println("Bienvenido!!, Seleccione el tipo de boleto correspondiente.");
+
+        do{
+            System.out.println("a = adulto, n = niño, i = Adulto mayor con (credencial del INAPAM), b = Adulto mayor");
+
+            tipos = entrada.next().charAt(0);
+            switch (tipos){
+                case 'a' -> {
+                    System.out.println("El boleto cuesta $ " + a);
+                    break;
+                }
+                case 'n' -> {
+                    System.out.println("El boleto cuesta $ " + n);
+                    break;
+                }
+                case 'i' -> {
+                    System.out.println("El boleto cuesta $ " + i);
+                    break;
+                }
+                case 'b' -> {
+                    System.out.println("El boleto cuesta $ " + b);
+                    break;
+                }
+            }
+            System.out.println("Otro boleto? (S/N)");
+            opcion =entrada.next().charAt(0);
+        }while(opcion == 's');
+        System.out.println("Compra hecha!");
+
     }
 
+
 }
+

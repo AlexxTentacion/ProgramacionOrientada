dd6a19ffc5b969e659d6c28e452d589f3a1ce27d
AlexxTentacion <89532931+AlexxTentacion@users.noreply.github.com>
2022-08-27 19:37:21 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,48 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("bye world");
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-}
+public class Main {
+    public static void main(String[] args) throws IOException {
+         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        int tamano;
+
+        System.out.println("Selecciona el tamaño de la piramide 1 = Chico, 2 = Mediano, 3 = Grande");
+        tamano = Integer.parseInt(entrada.readLine());
+
+
+        switch (tamano) {
+            case 1 -> {
+                System.out.println("*");
+                System.out.println("**");
+                System.out.println("***");
+            }
+            case 2 -> {
+                System.out.println("*");
+                System.out.println("**");
+                System.out.println("***");
+                System.out.println("****");
+                System.out.println("*****");
+                System.out.println("******");
+            }
+            case 3 -> {
+                System.out.println("*");
+                System.out.println("**");
+                System.out.println("***");
+                System.out.println("****");
+                System.out.println("*****");
+                System.out.println("******");
+                System.out.println("*******");
+                System.out.println("********");
+                System.out.println("*********");
+                System.out.println("**********");
+                System.out.println("***********");
+                System.out.println("************");
+            }
+            default -> System.out.println("Eso no es un tamaño");
+
+        }
+         }
+      }
+
